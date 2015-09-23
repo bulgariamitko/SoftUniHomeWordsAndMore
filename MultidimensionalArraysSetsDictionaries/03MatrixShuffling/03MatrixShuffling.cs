f72a61a6e4fcb2195c077b4ecb76666f43a54217
Dimitar <bulgaria_mitko@yahoo.com>
2015-09-23 16:14:51 +0300

    homework of multidimensionalArrays
    uploading the homework

---
+++ MultidimensionalArraysSetsDictionaries/03MatrixShuffling/03MatrixShuffling.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixRows = int.Parse(Console.ReadLine());
+            int matrixCols = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[matrixRows, matrixCols];
+
+            for (int rows = 0; rows < matrixRows; rows++)
+            {
+                for (int cols = 0; cols < matrixCols; cols++)
+                {
+                    matrix[rows, cols] = Console.ReadLine();
+                }
+            }
+
+            while (true)
+            {
+                List<string> input = Console.ReadLine().Split(' ').ToList();
+                if (input[0] == "swap")
+                {
+                    if ((int.Parse(input[1]) < matrixRows && int.Parse(input[3]) < matrixRows) && (int.Parse(input[2]) < matrixCols && int.Parse(input[4]) < matrixCols))
+                    {
+                        string swapFrom = matrix[int.Parse(input[1]), int.Parse(input[2])];
+                        string swapTo = matrix[int.Parse(input[3]), int.Parse(input[4])];
+                        // swaping
+                        matrix[int.Parse(input[1]), int.Parse(input[2])] = swapTo;
+                        matrix[int.Parse(input[3]), int.Parse(input[4])] = swapFrom;
+
+                        PrintMatrix(matrix);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                if (input[0] == "END")
+                {
+                    break;
+                }
+            }
+        }
+
+        static void PrintMatrix(string[,] filledMatrix)
+        {
+            for (int row = 0; row < filledMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < filledMatrix.GetLength(1); col++)
+                {
+                    Console.Write(filledMatrix[row, col] + " ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MultidimensionalArraysSetsDictionaries/04SequenceMatrix/04SequenceMatrix.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceInMatrix
+{
+    class SequenceInMatrix
+    {
+        static List<string> final = new List<string>();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Enter number of cols: ");
+            int m = int.Parse(Console.ReadLine());
+            string[,] matrix = new string[n, m];
+
+            FillMatrix(matrix);
+            MatrixHorizontal(matrix);
+            MatrixVertical(matrix);
+            MatrixDiagonal(matrix);
+            Console.WriteLine(String.Join(", ", final));
+        }
+
+        private static void MatrixDiagonal(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(1) - 1; i++)
+            {
+                List<string> tempList = new List<string>();
+                tempList.Add(matrix[0, i]);
+                for (int j1 = i + 1, j2 = i + 1; j1 < matrix.GetLength(0); j1++, j2++)
+                {
+                    if (matrix[j1, j2] == matrix[j1 - 1, j2 - 1])
+                    {
+                        tempList.Add(matrix[j1, j2]);
+                        if (j1 == matrix.GetLength(0) - 1)
+                        {
+                            if (tempList.Count > final.Count)
+                            {
+                                final.Clear();
+                                foreach (var item in tempList)
+                                {
+                                    final.Add(item);
+                                }
+                                tempList.Clear();
+                                tempList.Add(matrix[j1, j2]);
+                            }
+                            else
+                            {
+                                tempList.Clear();
+                                tempList.Add(matrix[j1, j2]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (tempList.Count > final.Count)
+                        {
+                            final.Clear();
+                            foreach (var item in tempList)
+                            {
+                                final.Add(item);
+                            }
+                            tempList.Clear();
+                            tempList.Add(matrix[j1, j2]);
+                        }
+                        else
+                        {
+                            tempList.Clear();
+                            tempList.Add(matrix[j1, j2]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void MatrixVertical(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                List<string> tempList = new List<string>();
+                tempList.Add(matrix[0, i]);
+                for (int j = 1; j < matrix.GetLength(0); j++)
+                {
+                    if (matrix[j, i] == matrix[j - 1, i])
+                    {
+                        tempList.Add(matrix[j, i]);
+                        if (j == matrix.GetLength(0) - 1)
+                        {
+                            if (tempList.Count > final.Count)
+                            {
+                                final.Clear();
+                                foreach (var item in tempList)
+                                {
+                                    final.Add(item);
+                                }
+                                tempList.Clear();
+                                tempList.Add(matrix[j, i]);
+                            }
+                            else
+                            {
+                                tempList.Clear();
+                                tempList.Add(matrix[j, i]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (tempList.Count > final.Count)
+                        {
+                            final.Clear();
+                            foreach (var item in tempList)
+                            {
+                                final.Add(item);
+                            }
+                            tempList.Clear();
+                            tempList.Add(matrix[j, i]);
+                        }
+                        else
+                        {
+                            tempList.Clear();
+                            tempList.Add(matrix[j, i]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void MatrixHorizontal(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                List<string> tempList = new List<string>();
+                tempList.Add(matrix[i, 0]);
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == matrix[i, j - 1])
+                    {
+                        tempList.Add(matrix[i, j]);
+                        if (j == matrix.GetLength(1) - 1)
+                        {
+                            if (tempList.Count > final.Count)
+                            {
+                                final.Clear();
+                                foreach (var item in tempList)
+                                {
+                                    final.Add(item);
+                                }
+                                tempList.Clear();
+                                tempList.Add(matrix[i, j]);
+                            }
+                            else
+                            {
+                                tempList.Clear();
+                                tempList.Add(matrix[i, j]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (tempList.Count > final.Count)
+                        {
+                            final.Clear();
+                            foreach (var item in tempList)
+                            {
+                                final.Add(item);
+                            }
+                            tempList.Clear();
+                            tempList.Add(matrix[i, j]);
+                        }
+                        else
+                        {
+                            tempList.Clear();
+                            tempList.Add(matrix[i, j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void FillMatrix(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("Enter string value: ");
+                    matrix[i, j] = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
