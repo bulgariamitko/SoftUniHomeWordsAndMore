39d626663dc45feb60e81e71d9604821c96bc881
Dimitar Klaturov <bulgaria_mitko@yahoo.com>
2016-04-19 11:26:14 +0300

    Issue Tracker - add new issue to project

--- 2.5AngularJS/Issue Tracker/app/issues/getIssues.js
+++ 2.5AngularJS/Issue Tracker/app/issues/getIssues.js
@@ -7,7 +7,43 @@ angular.module('issueTracker.issues.getIssues', []).factory('getIssues', ['$http
 		return deferred.promise;
 	}
 
+	function addIssue(issue) {
+		var deferred = $q.defer();
+		console.log(issue);
+		$http({
+		    method: 'POST',
+		    url: BASE_URL + 'Issues/',
+		    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+		    transformRequest: function(obj) {
+		        var str = [];
+		        for (var p in obj) {
+		        	if (p === 'labels') {
+	        			var labels = obj[p].split(',');
+	        			for (var i = 0; i < labels.length; i++) {
+		        			str.push('labels[' + i + '].name=' + encodeURIComponent(labels[i]));
+	        			}
+		        	} else if (p === 'duedate') {
+		        		var isoDate = new Date(obj[p]).toISOString();
+		        		str.push('duedate=' + isoDate);
+		        	} else {
+		        		str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+		        	}
+		        }
+		    	console.log(str.join("&"));
+		        return str.join("&");
+		    },
+		    data: issue
+		}).then(function(response) {
+			deferred.resolve(response.data);
+			console.log(response.data);
+		}, function(error) {
+			console.log(error);
+		});
+		return deferred.promise;
+	}
+
 	return {
-		getIssue: getIssue
+		getIssue: getIssue,
+		addIssue: addIssue
 	};
 }]);
--- 2.5AngularJS/Issue Tracker/app/issues/issues.js
+++ 2.5AngularJS/Issue Tracker/app/issues/issues.js
@@ -1,16 +1,43 @@
-angular.module('issueTracker.issues', ['issueTracker.issues.getIssues']).config(['$routeProvider', function($routeProvider) {
-	// $routeProvider.when('/issues', {
-	// 	templateUrl: 'app/issues/issues.html',
-	// 	controller: 'ProjectsController'
-	// });
-
+angular.module('issueTracker.issues', ['issueTracker.issues.getIssues', 'issueTracker.projects.getProjects', 'issueTracker.users.getUsers']).config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/issues/:id/', {
 		templateUrl: 'app/issues/issue.html',
 		controller: 'IssueController'
 	});
-}]).controller('IssueController', ['$scope', 'getIssues', '$routeParams', function($scope, getIssues, $routeParams) {
+
+	$routeProvider.when('/projects/:id/add-issue', {
+		templateUrl: 'app/issues/add.html',
+		controller: 'AddIssueController'
+	});
+}]).controller('IssueController', ['$scope', '$routeParams', 'getIssues', function($scope, $routeParams, getIssues) {
 	var id = $routeParams.id;
 	getIssues.getIssue(id).then(function(getIssueById) {
 		$scope.issue = getIssueById.data;
 	});
+}]).controller('AddIssueController', ['$scope', '$location', '$routeParams', 'getIssues', 'getProjects', 'getUsers', function($scope, $location, $routeParams, getIssues, getProjects, getUsers) {
+	getUsers.getAllUsers().then(function(allUsers) {
+		$scope.allUsers = allUsers;
+	});
+
+	var id = $routeParams.id;
+	getProjects.getProject(id).then(function(getProjectById) {
+		console.log(getProjectById.data);
+		console.log(getProjectById.data.Id);
+		$scope.newIssue = {
+			projectId: getProjectById.data.Id,
+			assigneeId: getProjectById.data.Lead.Id
+		};
+		$scope.getProjectById = getProjectById.data;
+	});
+
+	getProjects.allProjects().then(function(getallProjects) {
+		console.log(getallProjects);
+		$scope.getallProjects = getallProjects.data;
+	});
+
+	$scope.issue = function(issue) {
+		getIssues.addIssue(issue).then(function(addedIssue) {
+			console.log(addedIssue);
+			$location.path('/projects/' + id + '/');
+		});
+	};
 }]);
